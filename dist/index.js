87aa0649173e41638308e40cef25cb6950c90d4d
Dima <dimafilin6@icloud.com>
2022-10-29 21:51:54 +0700

    If I want to run process in the background, I dont want to wait process to terminate to finish my workflow

--- dist/index.js
+++ dist/index.js
@@ -116,18 +116,16 @@ function executeCommand(ssh, command) {
             }
             else {
                 console.log('Command seems to be a long running process');
-                let timeout;
                 yield ssh.exec(command, [], {
                     stream: "both",
                     onStdout(chunk) {
                         console.log(chunk.toString('utf8'));
-                        clearTimeout(timeout);
-                        timeout = setTimeout(() => {
-                            console.log('✅ Received feed back from the terminal. Process seem to be started.');
+                        console.log('✅ Received feed back from the terminal. Process seem to be started.');
+                        ssh.exec("disown", []).then(() => {
                             if (ssh.isConnected()) {
                                 ssh.dispose();
                             }
-                        }, 10000);
+                        });
                     },
                     onStderr(chunk) {
                         console.log(chunk.toString("utf-8"));
